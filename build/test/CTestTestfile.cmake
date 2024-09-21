fd4b471500c3e223191b8df037e7a0a31bc08bbc
starain-xinxin <Yxyhaha5@gmail.com>
2024-09-21 18:49:17 +0800

    test.sh complete

--- build/test/CTestTestfile.cmake
+++ build/test/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[EventPoolTest]=] "/home/yuanxinyu/CUDA_project/CUDA_API/build/test/EventPoolTest")
-set_tests_properties([=[EventPoolTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/yuanxinyu/CUDA_project/CUDA_API/test/CMakeLists.txt;14;add_test;/home/yuanxinyu/CUDA_project/CUDA_API/test/CMakeLists.txt;0;")
+set_tests_properties([=[EventPoolTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/yuanxinyu/CUDA_project/CUDA_API/test/CMakeLists.txt;15;add_test;/home/yuanxinyu/CUDA_project/CUDA_API/test/CMakeLists.txt;0;")
--- test.sh
+++ test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd build/test
-echo "----------- Now run the all executable -----------"
-for file in ./*; do
-  if [[ -x "$file" && -f "$file" ]]; then
-    echo " "
-    echo " running $file ... "
-    ./"$file"
-    echo " "
-  fi
-done
+ctest
+
